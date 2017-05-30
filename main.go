474c381678366ebd252c4fe06433a199440de059
Thiago Pontes <github@thiago.me>
2017-05-30 14:55:26 -0400

    init

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func echo(w http.ResponseWriter, r *http.Request) {
+	values := mux.Vars(r)
+	name := values["name"]
+	if name == "" {
+		name = "unknown"
+	}
+	w.Write([]byte(fmt.Sprintf("hello %s\n", name)))
+}
+
+func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/{name}", echo).Methods("GET")
+	router.HandleFunc("/", echo).Methods("GET")
+
+	http.ListenAndServe(":8000", router)
+}
