3e82e8acacb2e25fa62a53b96d8364dd054e162f
Francisco Souza <francisco.souza@nytimes.com>
2018-02-06 16:43:25 -0500

    travis: build docker image after running tests

---
+++ travis-scripts/docker.bash
@@ -0,0 +1,33 @@
+#!/bin/bash -el
+
+TRAVIS_MINOR_GO_VERSION=$(echo $TRAVIS_GO_VERSION | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
+GO_FOR_RELEASE=$(echo $GO_FOR_RELEASE | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
+	echo >&2 "Skipping image build on pull requests..."
+	exit 0
+fi
+
+if [ "${TRAVIS_MINOR_GO_VERSION}" != "${GO_FOR_RELEASE}" ]; then
+	echo >&2 "Skipping image build on Go ${TRAVIS_GO_VERSION}"
+	exit 0
+fi
+
+if [ "${TRAVIS_BRANCH}" != "master" ] && [ -n "${TRAVIS_TAG}" ]; then
+	echo >&2 "Skipping image build on branch ${TRAVIS_BRANCH}"
+	exit 0
+fi
+
+COMMIT_TAG=${TRAVIS_COMMIT:0:8}
+IMAGE_NAME=nytimes/video-captions-api
+
+docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+docker build -t ${IMAGE_NAME}:latest .
+
+docker tag ${IMAGE_NAME}:latest ${IMAGE_NAME}:${COMMIT_TAG}
+
+if [ -n "${TRAVIS_TAG}" ]; then
+	docker tag ${IMAGE_NAME}:${TRAVIS_TAG}
+fi
+
+docker push ${IMAGE_NAME}
