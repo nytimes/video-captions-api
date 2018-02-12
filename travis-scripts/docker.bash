36324893ca9ff1bdf83ac9158255125e97b7f83b
Francisco Souza <francisco.souza@nytimes.com>
2018-02-11 23:44:04 -0500

    travis: run Docker Hub integration as deployment step

--- travis-scripts/docker.bash
+++ travis-scripts/docker.bash
@@ -1,23 +1,5 @@
 #!/bin/bash -e
 
-TRAVIS_MINOR_GO_VERSION=$(echo $TRAVIS_GO_VERSION | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
-GO_FOR_RELEASE=$(echo $GO_FOR_RELEASE | sed -r 's/([0-9]+\.[0-9]+).*$/\1/')
-
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ]; then
-	echo >&2 "Skipping image build on pull requests..."
-	exit 0
-fi
-
-if [ "${TRAVIS_MINOR_GO_VERSION}" != "${GO_FOR_RELEASE}" ]; then
-	echo >&2 "Skipping image build on Go ${TRAVIS_GO_VERSION}"
-	exit 0
-fi
-
-if [ "${TRAVIS_BRANCH}" != "master" ] && [ -n "${TRAVIS_TAG}" ]; then
-	echo >&2 "Skipping image build on branch ${TRAVIS_BRANCH}"
-	exit 0
-fi
-
 COMMIT_TAG=${TRAVIS_COMMIT:0:8}
 IMAGE_NAME=nytimes/video-captions-api
 
